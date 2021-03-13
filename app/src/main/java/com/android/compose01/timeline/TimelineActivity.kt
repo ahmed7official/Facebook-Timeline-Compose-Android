92cc6931909faabbf0c6dc311d8ad8c660d84f34
ahmed7official <ahmed7official@gmail.com>
2021-03-13 22:03:19 +0200

    minor updates

--- app/src/main/java/com/android/compose01/timeline/TimelineActivity.kt
+++ app/src/main/java/com/android/compose01/timeline/TimelineActivity.kt
@@ -6,12 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
